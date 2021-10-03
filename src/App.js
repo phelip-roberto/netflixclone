b4c57b7f5933eb8ec4d9ff59d151f5dccf27a2c7
phelip-roberto <pheliproberto11@gmail.com>
2021-10-03 20:44:44 -0300

    integrate with api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Tmdb from './services/Tmdb';
+import MovieRow from './components/MovieRow';
+import './App.css';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
 
   useEffect(() => {
     const loadAll = async () => {
       let list = await Tmdb.getHomeList();
       console.log(list);
+      setMovieList(list);
     }
 
     loadAll();
   }, []);
 
   return (
-    <div>
-      Olá Mundo!
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <MovieRow key={key} title={item.title} items={item.items} />
+        ))}
+      </section>
     </div>
   )
 }
